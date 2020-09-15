36db94eedc2fed099ff04873a1b955f2cc8f024f
Ubiparip <ubiparip157@gmail.com>
2020-09-15 23:55:54 +0200

    cuvanje i citanje rezervacija iz fajla

---
+++ src/dao/RezervacijaDAO.java
@@ -0,0 +1,117 @@
+package dao;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
+import com.fasterxml.jackson.databind.type.MapType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+
+import model.Apartman;
+import model.Rezervacija;
+
+public class RezervacijaDAO {
+
+	private HashMap<UUID, Rezervacija> rezervacije = new HashMap<UUID, Rezervacija>();
+	private String path;
+	
+	public RezervacijaDAO(String path) {
+		this.path = path;
+		this.loadRezervacija("");
+	}
+
+	public HashMap<UUID, Rezervacija> getRezervacije() {
+		return rezervacije;
+	}
+
+	public void setRezervacije(HashMap<UUID, Rezervacija> rezervacije) {
+		this.rezervacije = rezervacije;
+	}
+
+	public void saveRezervacije(String contextPath) {
+		File f = new File(this.path + "\\data\\rezervacija.txt");
+		FileWriter fileWriter = null;
+		try {
+			fileWriter = new FileWriter(f);
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+			objectMapper.getFactory().configure(JsonGenerator.Feature.ESCAPE_NON_ASCII, true);
+			String sUsers = objectMapper.writeValueAsString(rezervacije);
+			fileWriter.write(sUsers);
+			fileWriter.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			if (fileWriter != null) {
+				try {
+					fileWriter.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void loadRezervacija(String contextPath) {
+		FileWriter fileWriter = null;
+		BufferedReader in = null;
+		File file = null;
+		try {
+			file = new File(this.path + "\\data\\rezervacija.txt");
+			in = new BufferedReader(new FileReader(file));
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.setVisibilityChecker(
+					VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
+			TypeFactory factory = TypeFactory.defaultInstance();
+			MapType type = factory.constructMapType(HashMap.class, UUID.class, Rezervacija.class);
+			objectMapper.getFactory().configure(JsonGenerator.Feature.ESCAPE_NON_ASCII, true);
+			rezervacije = ((HashMap<UUID, Rezervacija>) objectMapper.readValue(file, type));
+		} catch (FileNotFoundException fnfe) {
+			try {
+				file.createNewFile();
+				fileWriter = new FileWriter(file);
+				ObjectMapper objectMapper = new ObjectMapper();
+				objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+				objectMapper.getFactory().configure(JsonGenerator.Feature.ESCAPE_NON_ASCII, true);
+				String stringRezervacije = objectMapper.writeValueAsString(rezervacije);
+				fileWriter.write(stringRezervacije);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (fileWriter != null) {
+					try {
+						fileWriter.close();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	
+}
